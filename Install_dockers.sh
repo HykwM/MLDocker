aa40947e7ad53183554fadd503c2819e875b5e7a
HykwM <mst.hayakawa@icloud.com>
2020-04-28 09:14:15 +0900

    modify Install_dockers.sh

--- Install_dockers.sh
+++ Install_dockers.sh
@@ -20,8 +20,7 @@ echo "nvidia-container-toolkitおよびnvidia-container-runtimeをインスト�
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | sudo apt-key add -
-#curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list | sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
 sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit nvidia-container-runtime
 
@@ -37,11 +36,4 @@ sudo tee /etc/docker/daemon.json <<EOF
 EOF
 sudo pkill -SIGHUP dockerd
 
-
-
-echo "動作確認を行います。"
-sudo docker run --gpus all nvidia/cuda:10.0-base nvidia-smi
-
-
-
-echo "インストール作業が完了しました。"
+echo "インストール作業が完了しました。PCを再起動して下さい。"
